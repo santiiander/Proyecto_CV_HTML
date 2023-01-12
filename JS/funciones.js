c54331fdbd9c57fc3effbfaf04b14fa7a667df31
santiiander <santiagoandermatten1@gmail.com>
2023-01-11 22:48:54 -0300

    Finalizado

---
+++ JS/funciones.js
@@ -0,0 +1,15 @@
+document.getElementById("linin").addEventListener("click",function () {
+    window.open("https://www.linkedin.com/in/santiago-andermatten-1753531a2/")
+})
+document.getElementById("mail").addEventListener("click",function () {
+    window.open('mailto:santiagoandermatten1@gmail.com');
+    
+})
+
+document.getElementById("wsp").addEventListener("click",function () {
+    window.open("http://wa.me/+543472468850")
+})
+
+document.getElementById("ubic").addEventListener("click",function () {
+    window.open("https://goo.gl/maps/XHE9wVYqAAAPrJqu7")
+})
